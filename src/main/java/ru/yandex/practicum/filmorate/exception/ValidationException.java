d2394e4287ebd4cfbd995ec0ad2ea02aadb4d973
Safronov Igor <SafronovIgor1C@yandex.ru>
2024-05-06 23:35:01 +0300

    POSTMAN

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -6,5 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class ValidationException extends RuntimeException {
     public ValidationException() {
         super("ValidationException");
+        log.error("ValidationException", this);
     }
 }
